fa3b525dfd80cf77bacb05c636cd681883067c75
honda-pp <s.honda@keio.jp>
2023-04-10 22:30:24 +0900

    [add] logout button

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <div class="header">
+      <h1>{{ message }}</h1>
+      <button class="logout-btn" @click="logout">ログアウト</button>
+    </div>
   </div>
 </template>
 
 <script>
 import { inject } from 'vue';
+import router from '../router';
 
 export default {
   name: 'Home',
+  setup() {
+    const $cookies = inject('$cookies');
+    return {
+      $cookies,
+    };
+  },
   data() {
     return {
       message: '',
@@ -27,6 +37,11 @@ export default {
           console.error(error)
           this.message = 'エラーが発生しました'
         })
+    },
+    logout() {
+      this.$cookies.remove('id');
+      this.$cookies.remove('username');
+      router.push({ name: 'login' });
     }
   }
 }
