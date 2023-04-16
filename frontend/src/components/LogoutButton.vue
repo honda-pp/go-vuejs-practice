ff2f82b5aa84deea50f3479af9b6837c801195f6
honda-pp <s.honda@keio.jp>
2023-04-16 14:47:29 +0900

    [add] logoutButton.vue

---
+++ frontend/src/components/LogoutButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { inject } from 'vue';
+import router from '@/router';
+
+const cookies = inject('$cookies');
+
+const logout = () => {
+  cookies.remove('id');
+  cookies.remove('username');
+  router.push({ name: 'login' });
+}
+</script>
+
+<template>
+  <button class="logout-btn" @click="logout">ログアウト</button>
+</template>
+
+<style scoped>
+.logout-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 6px;
+}
+</style>
